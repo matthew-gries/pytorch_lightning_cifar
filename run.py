2091e646ca308123d58962abe0b013aefd3baece
matthew-gries <gries.m@husky.neu.edu>
2021-10-30 13:32:38 -0400

    small update to run to change epochs

--- run.py
+++ run.py
@@ -2,12 +2,13 @@ from data import get_data_loaders
 from classifier import CIFARClassifierModule
 import pytorch_lightning as pl
 
+MAX_EPOCHS = 10
 
 if __name__ == "__main__":
 
     train_loader, test_loader = get_data_loaders()
     model = CIFARClassifierModule()
-    trainer = pl.Trainer()
+    trainer = pl.Trainer(max_epochs=MAX_EPOCHS)
 
     trainer.fit(model, train_loader)
 
